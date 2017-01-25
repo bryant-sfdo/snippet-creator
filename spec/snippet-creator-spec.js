0a4924b496d44c415781a4178814cbd56d4311b8
ungb <ungb@github.com>
2017-01-24 23:03:42 -0800

    updating snippet and adding test

--- spec/snippet-creator-spec.js
+++ spec/snippet-creator-spec.js
@@ -15,8 +15,8 @@ describe('SnippetCreator', () => {
     activationPromise = atom.packages.activatePackage('snippet-creator');
   });
 
-  describe('when the snippet-creator:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
+  describe('when the snippet-creator:create event is triggered', () => {
+    it('shows the modal panel', () => {
       // Before the activation event the view is not on the DOM, and no panel
       // has been created
       expect(workspaceElement.querySelector('.snippet-creator')).not.toExist();
@@ -37,36 +37,7 @@ describe('SnippetCreator', () => {
 
         let snippetCreatorPanel = atom.workspace.panelForItem(snippetCreatorElement);
         expect(snippetCreatorPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'snippet-creator:toggle');
-        expect(snippetCreatorPanel.isVisible()).toBe(false);
-      });
-    });
 
-    it('hides and shows the view', () => {
-      // This test shows you an integration test testing at the view level.
-
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
-
-      expect(workspaceElement.querySelector('.snippet-creator')).not.toExist();
-
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'snippet-creator:created');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        // Now we can test for view visibility
-        let snippetCreatorElement = workspaceElement.querySelector('.snippet-creator');
-        expect(snippetCreatorElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'snippet-creator:toggle');
-        expect(snippetCreatorElement).not.toBeVisible();
       });
     });
   });
